41a030fbd96c021a2abc9a6c5cb83d04f1cb3946
tomynaveda-design <tomynaveda@gmail.com>
2026-09-11 11:10:29 -0300

    Arreglos bd

--- config.py
+++ config.py
@@ -8,8 +8,12 @@ class Config:
     # Clave de seguridad para formularios y sesiones
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave_por_defecto'
     
-    # URL de conexión a la base de datos
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # Obtener URL de base de datos
+    db_url = os.environ.get('DATABASE_URL')
     
-    # Desactivamos una característica de SQLAlchemy que consume mucha memoria
+    # Si DATABASE_URL no está configurada o si apunta a localhost, usará SQLite por defecto
+    if not db_url or 'localhost' in db_url or '127.0.0.1' in db_url:
+        db_url = 'sqlite:///gestion_obras.db'
+
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
